9333e9ccb0492305ad63e76ab7115f1918f3427c
hrishikeshsathyian <hrishikeshsathyiancoding@gmail.com>
2025-01-23 17:16:32 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Softess.java
@@ -0,0 +1,6 @@
+public class Softess {
+    public static void main(String[] args) {
+        String hello = "Hello! I'm Softess \nWhat can I do for you? \nBye. Hope to see you again soon!";
+        System.out.println(hello);
+    }
+}
