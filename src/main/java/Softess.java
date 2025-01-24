56300cd7789d21568367746d9e69ca29aa6d90ab
hrishikeshsathyian <hrishikeshsathyiancoding@gmail.com>
2025-01-24 15:08:11 +0800

    Mark as Done

--- src/main/java/Softess.java
+++ src/main/java/Softess.java
@@ -4,23 +4,30 @@ import java.util.Scanner;
 public class Softess {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Softess");
-        System.out.println("What can I do for you? \n");
-        String[] tasks = new String[100];
+        System.out.println("What can I do for you?\n");
+        Task[] tasks = new Task[100];
         Scanner scanner = new Scanner(System.in);
         int count = 0;
         while (true) {
-            String userInput = scanner.nextLine();
-            if (Objects.equals(userInput, "bye")) {
+            String fullUserInput = scanner.nextLine();
+            String[] userInput = fullUserInput.split(" ");
+
+            if (Objects.equals(userInput[0], "bye")) {
                 System.out.println("Softess: Bye. Hope to see you again soon noob!");
                 break;
-            } else if (Objects.equals(userInput, "list")) {
+            } else if (Objects.equals(userInput[0], "list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println(String.format("%d. [%s] %s", (i + 1), tasks[i].getStatusIcon(), tasks[i].description));
                 }
-                System.out.println("\n");
+            } else if (Objects.equals(userInput[0], "mark")) {
+                int num = Integer.valueOf(userInput[1]);
+                tasks[num - 1].markAsDone();
+            } else if (Objects.equals(userInput[0], "unmark")) {
+                int num = Integer.valueOf(userInput[1]);
+                tasks[num - 1].markAsUnDone();
             } else {
-                System.out.println("added: " + userInput + "\n");
-                tasks[count] = userInput;
+                System.out.println("added: " + fullUserInput + "\n");
+                tasks[count] = new Task(fullUserInput);
                 count++;
             }
 
@@ -29,4 +36,3 @@ public class Softess {
         scanner.close();
     }
 }
-
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done: \n [X] " + this.description);
+    }
+
+    public void markAsUnDone() {
+        this.isDone = false;
+        System.out.println("Nice! I've marked this task as undone: \n [ ] " + this.description);
+    }
+
+}
